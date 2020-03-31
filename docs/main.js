36788728a016feb4c6a55ecb28df4489c17a996d
Tim McMaster <tim@mcmaster.id.au>
2020-03-31 11:25:26 +1100

    checkpoint

--- docs/main.js
+++ docs/main.js
@@ -9,6 +9,45 @@ let sites = {
     'docs': 'https://github.com/tmcmaster/tm-receipt#readme'
 };
 
+const sageDetails = `
+#Susan Smith
+Consultant
+- ABN: 11 222 333 444
+- IBCLC: L-12345
+~blank~
+- Medicare Provider Number: 123456A
+~blank~
+- Health Fund Reference Numbers:
+  + Medibank: 123456B
+  + Bupa: 123456C
+  + NIB: 123456D
+  + HCF: 123456E
+`;
+
+const data = {
+    title: 'Sage.fm',
+    subtitle1: 'Sage.fm Pty Ltd',
+    subtitle2: 'ABN 11 222 333 444',
+    heading: 'Conversation Receipt',
+    subHeading: 'Jack Jones',
+    details: {
+        'Conversation Details': {
+            'Date': '2020-03-19',
+            'Time': '17.22pm',
+            'Conversation Length': '0:13:28secs',
+            'Sage Details': sageDetails
+        },
+        'Payment Details': {
+            '10-min minimum rate': '**A$40.00**',
+            'Every 1 min after': '**A$4.00**',
+            '**TOTAL**': '**A$56.00**'
+        }
+    },
+    footer: 'Thank you'
+};
+
+console.log('DATA', JSON.stringify(data, null, ' '));
+
 render(html`
     <style>
         body {
@@ -16,13 +55,36 @@ render(html`
           margin: 0;
         } 
         
-        html {
-            --tm-receipt-font-family: sage;
+        textarea{
+            width: 300px;
+            height: 300px;
+        }
+        
+        tm-receipt{
+            margin-left: 20px;
+            width: 500px;
+
         }
     </style>
     <tm-examples heading="tm-receipt" .sites="${sites}">
         <section title="Example">
-            <tm-receipt></tm-receipt>
+            <style>
+                tm-receipt {
+                    --tm-receipt-font-family: sage;
+                    --tm-receipt-color: rgb(16,25,64);
+                    --tm-receipt-section-heading-color: red;
+                    --tm-receipt-section-heading-1-color: blue;
+                    
+                    --tm-receipt-section-1-key-1-color: green;
+                    --tm-receipt-section-1-key-2-color: purple;
+                    
+                    --tm-receipt-section-1-value-1-color: red;
+                    --tm-receipt-section-1-value-2-color: orange;
+                }
+            </style>
+            <textarea id="markdown">${JSON.stringify(data, null, ' ')}</textarea>
+            <tm-receipt .data="${data}"></tm-receipt>
         </section>
     </tm-examples>
+    
 `, document.querySelector('body'));
--- src/tm-receipt.js
+++ src/tm-receipt.js
@@ -1,29 +1,93 @@
 import {html} from 'lit-html';
 import {LitElement, css} from 'lit-element';
 
+import {htmlLiteral} from 'https://unpkg.com/@polymer/polymer@3.0.5/lib/utils/html-tag.js'
+
 window.customElements.define('tm-receipt', class extends LitElement {
 
     // noinspection JSUnusedGlobalSymbols
     static get properties() {
         return {
-            heading: {type: String}
+            data: {type: Object}
         }
     }
 
     constructor() {
         super();
-        this.heading = 'Hello Sage!';
+        this.conv = new showdown.Converter({noHeaderId: true});
+        this.template = document.createElement('template');
     }
 
     static get styles() {
         // language=CSS
         return css `
             :host {
-              display: inline-block;
-            }
-            h2 {
+                display: inline-block;
+                border: solid grey 1px;
+                padding: 20px;
+                color: black;
                 font-family: var(--tm-receipt-font-family, inherit);
-                color: gray;
+                
+                /** Colors **/
+                
+                --section-heading-color: var(--tm-receipt-section-heading-color, inherit);
+                --section-key-color: var(--tm-receipt-section-key-color, inherit);
+                --section-value-color: var(--tm-receipt-section-value, inherit);
+
+                --section-1-key-color: var(--tm-receipt-section-key-color, var(--section-key-color));
+                --section-1-value-color: var(--tm-receipt-section-value, var(--section-value-color));
+
+                --section-heading-1-color: var(--tm-receipt-section-heading-1-color, var(--section-heading-color));
+                --section-heading-2-color: var(--tm-receipt-section-heading-2-color, var(--section-heading-color));
+                
+                --section-1-key-1-color: var(--tm-receipt-section-1-key-1-color, var(--section-1-key-color));
+                --section-1-key-2-color: var(--tm-receipt-section-1-key-2-color, var(--section-1-key-color));
+                --section-1-key-3-color: var(--tm-receipt-section-1-key-3-color, var(--section-1-key-color));
+
+                --section-1-value-1-color: var(--tm-receipt-section-1-value-1-color, var(--section-1-value-color));
+                --section-1-value-2-color: var(--tm-receipt-section-1-value-2-color, var(--section-1-value-color));
+                --section-1-value-3-color: var(--tm-receipt-section-1-value-3-color, var(--section-1-value-color));
+
+                --section-2-key-1-color: var(--tm-receipt-section-2-key-1-color, var(--section-2-key-color));
+                --section-2-key-2-color: var(--tm-receipt-section-2-key-2-color, var(--section-2-key-color));
+                --section-2-key-3-color: var(--tm-receipt-section-2-key-3-color, var(--section-2-key-color));
+
+                --section-2-value-1-color: var(--tm-receipt-section-2-value-1-color, var(--section-2-value-color));
+                --section-2-value-2-color: var(--tm-receipt-section-2-value-2-color, var(--section-2-value-color));
+                --section-2-value-3-color: var(--tm-receipt-section-2-value-3-color, var(--section-2-value-color));
+
+            }
+
+            ul {
+                list-style-type:none;
+            }
+            section {
+                padding: 20px;
+                border: solid grey 1px;
+            }
+            
+            .section-heading-1 {
+                color: var(--section-heading-1-color, inherit);
+            }
+            .section-heading-2 {
+                color: var(--section-heading-2-color, inherit);
+            }
+            .section-heading-3 {
+                color: var(--section-heading-3-color, inherit);
+            }
+
+            .section-1-key-1 {
+                color: var(--section-1-key-1-color, inherit);
+            }
+            .section-1-key-2 {
+                color: var(--section-1-key-2-color, inherit);
+            }
+
+            .section-1-value-1 {
+                color: var(--section-1-value-1-color, inherit);
+            }
+            .section-1-value-2 {
+                color: var(--section-1-value-2-color, inherit);
             }
         `;
     }
@@ -31,7 +95,40 @@ window.customElements.define('tm-receipt', class extends LitElement {
     // noinspection JSUnusedGlobalSymbols
     render() {
         return html`
-          <h2>${this.heading}</h2>
+            <template id="a"><h3>sdfasdfasf</h3></template>
+            <header>
+                <img/>
+                <h1>${this.data.title}</h1>
+                <div>
+                    <span>${this.data.subtitle1}</span>
+                    <span>${this.data.subtitle2}</span>
+                </div>
+            </header>
+            <main>
+                <h2>${this.data.heading}</h2>
+                <h3>${this.data.subHeading}</h3>
+                ${Object.keys(this.data.details).map((key, i) => html`
+                    <section>
+                        <h3 class="section-heading-${i+1}">${key}</h3>
+                        <table>
+                            ${Object.keys(this.data.details[key]).map((k,j) => html`
+                                <tr>
+                                    <td class="section-${i+1}-key-${j+1}">${this.formatText(k)}</td>
+                                    <td class="section-${i+1}-value-${j+1}">${this.formatText(this.data.details[key][k])}</td>
+                                </tr>
+                            `)}
+                        </table>
+                    </section>
+                `)}
+                <footer>${this.data.footer}</footer>
+            </main>
         `;
+    }
+
+    formatText(text) {
+        const htmlString = this.conv.makeHtml(text.replace(/~blank~/gi, '<p></p>'));
+        this.template = document.createElement('template');
+        this.template.innerHTML = htmlString;
+        return this.template.content;
     }
 });
